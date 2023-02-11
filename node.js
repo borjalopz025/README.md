5e7a011078f65926cf4608c9a055e6a819ca3d65
borjalopez025 <borjaswim1995@gmail.com>
2023-02-11 15:52:52 +0100

    guardado

---
+++ node.js
@@ -0,0 +1,20 @@
+function suma( num1,num2){
+    num1 + num2
+  }suma()
+
+  function resta (num1,num2){
+    return num1 - num2
+  }
+
+  function multiplicacion (num1,num2){
+    return num1 * num2
+  }
+
+  function division ( num1, num2){
+    return num1 / num2
+  }
+
+   function cuadradoNumero(num1,num2){
+    return num1 ** num2
+   }
+  module.exports = {suma,resta,multiplicacion,division,cuadradoNumero}
